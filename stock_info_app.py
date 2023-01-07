d8a967159047b91965d1f14f1a48527033915738
codener_choux <108624466+codenerchoux@users.noreply.github.com>
2023-01-07 12:25:26 +0900

    edit file

--- stock_info_app.py
+++ stock_info_app.py
@@ -6,57 +6,63 @@ import matplotlib.pyplot as plt
 import matplotlib
 from io import BytesIO
 
-#----------------------------------------
+# ----------------------------------------
 # 한국 주식 종목 코드를 가져오는 함수
-#----------------------------------------
+# ----------------------------------------
+
+
 def get_stock_info(market_type=None):
     base_url = "https://kind.krx.co.kr/corpgeneral/corpList.do"
     method = "download"
 
-    if market_type=='kospi':
-      marketType = 'stockMkt'
-    elif market_type=='kosdaq':
-      marketType = 'kosdaqMkt'
+    if market_type == 'kospi':
+        marketType = 'stockMkt'
+    elif market_type == 'kosdaq':
+        marketType = 'kosdaqMkt'
     else:
-      marketType =''
-    
-    url = "{0}?method={1}&marketType={2}".format(base_url,method,marketType)
+        marketType = ''
+
+    url = "{0}?method={1}&marketType={2}".format(base_url, method, marketType)
 
     df = pd.read_html(url, header=0)[0]
 
     # 종목코드 열을 6자리 숫자로 표시된 문자열로 변환
     df['종목코드'] = df['종목코드'].apply(lambda x: f"{x:06d}")
-    df = df[['회사명','종목코드']]
+    df = df[['회사명', '종목코드']]
 
     return df
 
 
-#----------------------------------------------------
+# ----------------------------------------------------
 # yfinance에 이용할 Ticker 심볼을 반환하는 함수
-#----------------------------------------------------
+# ----------------------------------------------------
 def get_ticker_symbol(company_name, market_type):
     df = get_stock_info(market_type)
-    
-    code = df[df['회사명']==company_name]['종목코드'].values
+
+    code = df[df['회사명'] == company_name]['종목코드'].values
     code = code[0]
 
-    if market_type=='kospi':
-      ticker_symbol = code + ".KS"
-    elif market_type=='kosdaq':
-      ticker_symbol = code + ".KQ"
+    if market_type == 'kospi':
+        ticker_symbol = code + ".KS"
+    elif market_type == 'kosdaq':
+        ticker_symbol = code + ".KQ"
 
     return ticker_symbol
+
 
 st.title("주식 정보를 가져오는 웹앱")
 
 st.sidebar.header("회사 이름과 기간 입력")
-stock_name = st.sidebar.text_input('회사 이름',value="NAVER")
-date_range = st.sidebar.date_input('기간',value=[datetime.date(2019,1,1),datetime.date(2021,12,31)])
+stock_name = st.sidebar.text_input('회사 이름', value="NAVER")
+type_options = ['kospi', 'kosdaq']
+stock_type = st.sidebar.radio('코스피/코스닥', type_options)
+date_range = st.sidebar.date_input(
+    '기간', value=[datetime.date(2019, 1, 1), datetime.date(2021, 12, 31)])
 
 clicked = st.sidebar.button('주가 데이터 가져오기')
 if clicked:
-    # 인자로 주식 종목 이름(stock_name)과 마켓 타입(kospi) 전달
-    ticker_symbol = get_ticker_symbol(stock_name, 'kospi')
+    # 인자로 주식 종목 이름(stock_name)과 마켓 타입(stock_type) 전달
+    ticker_symbol = get_ticker_symbol(stock_name, stock_type)
 
     # 인자로 ticker_symbol 전달
     ticker_data = yf.Ticker(ticker_symbol)
@@ -65,7 +71,7 @@ if clicked:
     start_p = date_range[0]
     end_p = date_range[1]+datetime.timedelta(days=1)
 
-    df = ticker_data.history(start=start_p,end=end_p)
+    df = ticker_data.history(start=start_p, end=end_p)
 
     st.subheader(f"[{stock_name}] 주가데이터")
     st.dataframe(df.head())
@@ -83,9 +89,9 @@ if clicked:
     fig = ax.get_figure()
 
     st.pyplot(fig)
-    
+
     st.markdown("**주가 데이터** 다운로드")
-    
+
     csv_data = df.to_csv()
 
     excel_data = BytesIO()  # 메모리 버퍼에 바이너리 객체 생성
@@ -95,8 +101,9 @@ if clicked:
     df.to_excel(excel_data)  # DataFrame 데이터를 엑셀 형식으로 버퍼에 쓰기
 
     colums = st.columns(2)
-    with colums[0] :
-      st.download_button('CSV 파일 다운로드', csv_data, file_name='stock_data.csv')
-    
+    with colums[0]:
+        st.download_button('CSV 파일 다운로드', csv_data, file_name='stock_data.csv')
+
     with colums[1]:
-      st.download_button('Excel 파일 다운로드',excel_data,file_name='excel_data.csv')
+        st.download_button('Excel 파일 다운로드', excel_data,
+                           file_name='excel_data.csv')
